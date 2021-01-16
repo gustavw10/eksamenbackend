4a7c7b6dd5ec9a231f831cbfdaa30beaf3e9f307
Gustav <cph-gw30@cphbusiness.dk>
2021-01-16 20:07:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# caFINAL
+
+Dette er backend
+
+Front-enden er her: https://github.com/gustavw10/reactsecure/blob/main/src/App.js og fetcher 3 forskellige endpoints som henter information fra eksterne servere
+
+Dertil er der en knap til at gemme informatiom om et endpoint i en database dedikeret til det
+
+Travis build: https://travis-ci.com/github/gustavw10/caFINAL
---
+++ src/main/java/rest/UserResource.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.UsersDTO;
+import entities.User;
+import facades.UserFacade;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+import utils.EMF_Creator;
+import utils.HttpUtils;
+
+/**
+ * REST Web Service
+ *
+ * @author maddy
+ */
+
+
+@Path("info")
+public class UserResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+       
+    private static final UserFacade FACADE =  UserFacade.getUserFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+    
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World, with startcode test\"}";
+    }
+    
+    @Path("count")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getRenameMeCount() {
+        long count = FACADE.getUserCount();
+        //System.out.println("--------------->"+count);
+        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    }
+    
+    
+    @GET
+    @Path("all")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllUsers() {
+        UsersDTO users = FACADE.getAllUsers();
+        return GSON.toJson(users);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("allCount")
+    public String allUsers() {
+
+        EntityManager em = EMF.createEntityManager();
+        try {
+            TypedQuery<User> query = em.createQuery ("select u from User u",entities.User.class);
+            List<User> users = query.getResultList();
+            return "[" + users.size() + "]";
+        } finally {
+            em.close();
+        }
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed({"user", "admin"})
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello, user. You are logged into '" + thisuser + "'\"}";
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
+    
+}
+
